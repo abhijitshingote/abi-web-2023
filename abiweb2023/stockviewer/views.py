00d436fa05bcdecfbd73c4d8717522b49209a39d
Abhijit Shingote <abhijitshingote@Abhijits-MBP.lan>
2023-11-24 12:56:59 -0500

    added yahoo news scraper

--- abiweb2023/stockviewer/views.py
+++ abiweb2023/stockviewer/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from .models import StockPriceHistory
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
+
+def get_news(symbol):
+    url = f"https://finance.yahoo.com/quote/{symbol}?p={symbol}&.tsrc=fin-srch"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    news=[]
+    for link in soup.find_all(id='mrt-node-quoteNewsStream-0-Stream'):
+        for l in link.find_all('a'):
+            news.append(
+                {
+                    'text': l.text,
+                    'link': 'https://finance.yahoo.com'+l['href']
+                }
+            )
+    return news
+
 
 def list_stocks(request):
     stocks=StockPriceHistory.objects.using("stocks").all()
-    symbols=list(set([stock.symbol for stock in stocks]))
+    symbols=sorted(list(set([stock.symbol for stock in stocks])))
     return render(request, "stockviewer/liststocks.html",{'symbols':symbols})
 
 def stock_detail(request,symbol):
+    print(get_news(symbol))
     prices=StockPriceHistory.objects.using("stocks").filter(symbol=symbol).order_by('date')
     dates,closes=[],[]
     for p in prices:
@@ -17,6 +36,7 @@ def stock_detail(request,symbol):
         'closes':closes,
         'dates':dates,
         'symbol':symbol,
-        'last_price':prices.reverse()[0]
+        'last_price':prices.reverse()[0],
+        'news':get_news(symbol)
     }
     return render(request, "stockviewer/stock_detail.html",context)
